889ca2d036edcbf1baf4e3b54f8c39bf384f07bf
Vanshaj Bhardwaj <vanshajbhardwaj689@gmail.com>
2026-03-30 01:16:29 +0530

    Update fullProg.py

--- fullProg.py
+++ fullProg.py
@@ -1,109 +1,95 @@
-import mysql.connector
-
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="5126",
-    database="libMngmtSys"
-)
-
-cursor = conn.cursor()
-cursor.execute("INSERT INTO books(ID,Title,Author,Genre,Language,Status) VALUES(%s,%s,%s,%s,%s,%s)",
-               ("101", "Demo Book", "Test Author", "Fiction", "English", False)
-               )
-
-conn.commit()
-
-print("Book inserted successfully")
-
 library = []
 
 def addBook():
-    bid = input("Enter Book ID:").strip()
+    bid  = input("Enter Book ID:").strip()
     for book in library:
         if book["id"] == bid:
-            print("This Book ID already exists, Try Different ID X(")
+            print("\nID already exist, Try New ID")
             return
     ttl = input("Enter Book Title:")
     auth = input("Enter Author Name:")
-    gen = input("Enter Genre:")
-    lan = input("Enter Language:")
+    status = False
 
-    book = { "id":bid, "ttl":ttl, "auth":auth, "gen":gen, "lan":lan, "status": False }
-
+    book = {"id":bid,"ttl":ttl,"auth":auth,"status":status}
     library.append(book)
 
-    print("Book Added Successfully!")
-    print()
+    print("\nBook Added Successfully")
 
 def viewBook():
     if not library:
-        print("Sorry library is empty :(")
-
-    print("\n---List Of Books In Library---")
-    for book in library:
-        status = "Issued" if book["status"] else "Available"
-        print(f' Book ID = {book["id"]} | Title = {book["ttl"]} | Author = {book["auth"]} | Genre = {book["gen"]} | Language = {book["lan"]} | Status = {status}')
+        print("\nLibrary Empty")
+        return
+    print("\n---Books In Library---")
     print()
+    for book in library:
+        status = "Issued" if book["status"] else "Avaiable"
+        print(f"Book ID = {book["id"]} | Book Title = {book["ttl"]} | Author = {book["auth"]} | Status = {status}")
+        print()
 
 def issueBook():
-    bid = input("Enter Book ID:")
+    bid = input("\nEnter Book ID:").strip()
+    for book in library:
+        if book["id"]==bid:
+            if book["status"]:
+                print("\nBook Already Issued")
+                return
+            book["status"] = True
+            print("\nBook Issued Successfully")
+            return
+
+    print("\nNo Book Records")
+
+
+def returnBook():
+    bid = input("\nEnter Book ID:")
     for book in library:
         if book["id"] == bid:
             if not book["status"]:
-                book["status"] = True
-                print("\nBook Issued Successfully!!")
-            else:
-                print("\nBook Already Issued :(")
+                print("\nBook Was Not Issued")
+                return
+            book["status"] = False
+            print("\nBook Returned Successfully")
             return
+    print("\nNo Records Found")
 
-    print("\nNo Records Found X(")
-    print()
+def searchBook():
+    key = input("Enter Book ID or Title:").strip().lower()
+    if not library:
+        print("\nNo Record Found")
+        return
+    for book in library:
+        if book["id"]==key or book["ttl"].lower() == key:
+            print("\nSearch Results:")
+            status = "Issued" if book["status"] else "Avaiable"
+            print(f"\nBook ID = {book["id"]} | Book Title = {book["ttl"]} | Author = {book["auth"]} | Status = {status}")
 
-def returnBook():
+def deleteBook():
     bid = input("Enter Book ID:")
+
     for book in library:
         if book["id"] == bid:
             if book["status"]:
-                book["status"] = False
-                print("\nBook Returned Successfully!!")
-            else:
-                print("\nThis Book Was Not Issued :(")
+                print("\nBook Is Issued, Return Book")
+                return
+            library.remove(book)
+            print("\nBook Deleted Successfully")
             return
-    print("\nNo Records Found X(")
-    print()
-
-def searchBook():
-    if not library:
-        print("No Books in Library")
-        return
-    key = input("Enter Book ID or Book Title:").strip().lower()
-    found = False
-    for book in library:
-        if book["id"] == key or key in book["ttl"].lower():
-            if not found:
-                print("\nBooks Found--->")
-                found = True
-            status = "Issued" if book["status"] else "Available"
-            print(f'\nBook ID = {book["id"]} | Title = {book["ttl"]} | Author = {book["auth"]} | Genre = {book["gen"]} | Language = {book["lan"]} | Status = {status}')
-    if not found:
-        print("No Records Found")
-    print()
-
+    print("\nNo Records Found")
 
 def menu():
     while True:
-        print("\n===Welcome To Library Management System===")
-        print("\nChoose Your Option--->")
+        print("--------------------------------------------")
+        print("===Welcome To Library Management System===")
+        print("\n Choose Operation--->")
         print("---> 1. Add Book")
         print("---> 2. View Book")
         print("---> 3. Issue Book")
         print("---> 4. Return Book")
         print("---> 5. Search Book")
-        print("---> 6. Exit The Menu")
+        print("---> 6. Delete Book")
+        print("---> 7. Exit LMS")
 
-        ch = input("Enter Your Choice:")
-        print()
+        ch = input("\nChoose Your Option:")
         if ch == "1":
             addBook()
         elif ch == "2":
@@ -115,9 +101,26 @@ def menu():
         elif ch == "5":
             searchBook()
         elif ch == "6":
-            print("\nExiting The Menu...")
-            break
+            deleteBook()
+        elif ch == "7":
+            print("\nExiting The System")
         else:
-            print("\nInvalid Choice")
+            print("Invalid Choice")
 
 menu()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
